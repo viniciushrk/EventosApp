e7257cda6933c95518ca617c3682249ee7332c6b
01467545228 <01467545228>
2022-05-02 07:58:26 -0400

    feat: implements card Pokemon, read archive json

---
+++ EventoApp.Core/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventoApp.Core.Models;
+public class Pokemon
+{
+    public string Nome { get; set; }
+    public string Especie { get; set; }
+    public string Raridade { get; set; }
+    public string Img { get; set; }
+}
+
